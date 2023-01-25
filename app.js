3f56386971b8ee8d34fcac221ee4e4f44292506f
Shone <vulicnebojsa994@gmail.com>
2023-01-25 02:59:06 +0100

    sending data throug form

--- app.js
+++ app.js
@@ -13,12 +13,28 @@ app.set('port', process.env.PORT || 8000);
 //set static shared folder
 app.use(express.static(__dirname+'/public'));
 
+//enable Post method 
+app.use(require('body-parser')());
+
 /*
 *
 *   PATHS GOES HIRE
 */
 app.get('/', function(req, res){
   res.render('test');
+});
+
+//forms path
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
+app.get('/thank-you', function(req, res){
+  res.render('thank-you');
 });
 
 /*
