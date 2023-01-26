53ab4371388eaa217ceb899bd77110de07bc4919
Shone <vulicnebojsa994@gmail.com>
2023-01-26 02:43:05 +0100

    tried with formidable

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const formidable = require('formidable');
 
 //seting template handlebars
 const handlebars = require('express3-handlebars').create({
@@ -58,6 +59,35 @@ app.get('/thank-you', function(req, res){
   res.render('thank-you');
 });
 
+//form with file upload
+app.get('/contest/vocation-photo', function(req, res){
+  const now = new Date();
+  res.render('fileUploads', {
+    year: now.getFullYear(), month: now.getMonth()
+  });
+});
+
+app.post('/contest/vocation-photo-sent', function(req, res){
+  const form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    if(err) return res.redirect(303, '/error');
+    console.log('received fields:');
+    console.log(fields);
+    console.log('RECEIVED FILES:');
+    console.log(files);
+    res.redirect(303, '/thank-you');
+    /*
+    res.render('fields', {
+      fields: fields,
+      files, files
+    })*/
+  });
+});
+
+
+app.get('/error', function(req, res){
+  res.render('404');
+});
 /*
 *
 *   ERRORS HANDLING
