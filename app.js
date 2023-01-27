809d0e48bf600485b4c5266529ef30d454491508
Shone <vulicnebojsa994@gmail.com>
2023-01-27 03:17:20 +0100

    Session set up done

--- app.js
+++ app.js
@@ -21,16 +21,29 @@ app.set('view engine', 'handlebars');
 //body-parser for (POST request)
 app.use(require('body-parser')());
 
-//add cookie parser 
+//
+//add cookie parser and session
+//
 app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
+
+//clearing flesh message
+app.use(function(req, res, next){
+  //if there's a flash message, transfer
+  //it to the context, then clear it
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 //set port
 app.set('port', process.env.PORT || 8000);
-//set static shared folder
+
+//set path to the static shared folder
 app.use(express.static(__dirname+'/public'));
 
 /*
-*
+*   PATHS
 *   PATHS GOES HIRE
 */
 app.get('/', function(req, res){
@@ -51,7 +64,7 @@ app.post('/getCookie', function(req, res){
   if(ck != '')
   res.cookie('monster', ck);
   if(sck != '')
-  res.cookie('signed_monster', sck, {signed: true});
+  res.cookie('signed_monster', sck, {signed: true, maxAge: 1000 * 60 * 60 *24*2});
 
 
   res.redirect(303, '/getCookie');
@@ -61,7 +74,9 @@ app.post('/getCookie', function(req, res){
 app.get('/getCookie', function(req, res){
   res.render('cookies/getCookie', {
     cookie: req.cookies.monster,
-    signedCookie: req.signedCookies.signed_monster
+    signedCookie: req.signedCookies.signed_monster,
+    spec: req.signedCookies.spec,
+    path: req.cookies.path
   });
 });
 
@@ -71,6 +86,76 @@ app.get('/deleteCookie', function(req, res){
 app.post('/deleteCookie', function(req, res){
   res.clearCookie(req.body.cookieName);
   res.redirect(303, '/getCookie');
+});
+
+
+//set cookie with spec parameters and path
+app.post('/createSpecCookie', function(req,res){
+  const fc = req.body.specCookie;
+  const sc = req.body.pathCookie;
+  if(fc){
+    res.cookie('spec', fc, {
+      signed: true,
+      httpOnly: true,
+      secure: true 
+    });
+  }
+  if(sc){
+    res.cookie('path', sc, {
+      maxAge: 1000*60*60*24,
+      path: '/only/this/path'
+    });
+  }
+  res.redirect('/getCookie');
+});
+
+app.get('/pathCookie', function(req, res){
+  res.render('test', {
+    pathc: req.cookies.path
+  });
+});
+
+app.get('/only/this/path', function(req, res){
+  res.render('test', {pathc: req.cookies.path});
+});
+
+//
+// Sessions
+app.get('/session', function(req, res){
+  req.session.userName = 'Anonymous';
+  req.session.colorScheme = 'text-success';
+  let colorScheme = req.session.colorScheme || 'dark';
+  res.render('cookies/session/getSession', {
+    userName: req.session.userName,
+    color: req.session.colorScheme
+  });
+});
+//flash messages
+app.get('/invalidemail', function(req, res){
+  req.session.flash = {
+    type: 'danger', 
+    intro: 'Validation error!', 
+    message: 'The email address you entered was not valid.',
+  };
+  res.redirect(303, '/');
+});
+
+app.get('/databaseerror', function(req, res){
+  req.session.flash = {
+    type: 'danger', 
+    intro: 'Database error!',
+    message: 'There was a database error; please try again later.',
+  };
+  res.redirect('/');
+});
+
+app.get('/message', function(req, res){
+  req.session.flash = {
+    type: 'success',
+    intro: 'Thank you',
+    message: 'You have now been signed up for the newsletter.',
+  };
+  res.redirect(303, '/');
 });
 
 /*
